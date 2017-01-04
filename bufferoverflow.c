e329c4c319bb18009d420c5581bdf96c7b8b8158
Zak Alfonse <Zalfonse@ruelala.com>
2017-01-04 11:10:27 -0500

    Update bufferoverflow.c

--- bufferoverflow.c
+++ bufferoverflow.c
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
   char pwdstr[12];
   char targetpwd[12] = "MyPwd123";
 
+  // `gets` will write past the 12 bytes we allocated for pwdstr if the string is larger
   gets(pwdstr);
 
   if(strncmp(pwdstr, targetpwd, 12) == 0)
